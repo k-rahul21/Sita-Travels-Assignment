437510732e7ca803bb9f528fe3cde51c0278326a
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2022-10-15 23:01:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PostsList from "./components/PostsList";
 
 function App() {
+  const dummyPosts = [
+    {
+      id: 1,
+      title: "dp",
+      caption: "jhdfuhas",
+    },
+    {
+      id: 2,
+      title: "picture",
+      caption: "dfhuias",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <button>Fetch Posts</button>
+      </section>
+      <section>
+        <PostsList posts={dummyPosts} />
+      </section>
     </div>
   );
 }
