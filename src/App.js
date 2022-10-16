e6f252f84cee2f9fa57fa4e6b0f155571c369ecd
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2022-10-17 01:41:33 +0530

    fix typos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import AddPost from "./components/AddPost";
 import PostsList from "./components/PostsList";
